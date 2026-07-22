1a53672d861f107682bfa1a07ab77f9e1e4b450a
Manuel Cortes <manny.cortes307@gmail.com>
2026-07-21 21:04:29 -0600

    main.py added

---
+++ main.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+#post is when a user is providing server with a file 
+app.post("/upload")
+#File parameter tells fast api not to expect a json
+#"..." tells fastapi not to do anything if not a file type
+async def main(file: UploadFile = File(...)):
+    stream = file.file
+    print(stream.file)
+
+
+
+
+if "__name__" == "__main__":
+    main()
